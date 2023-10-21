b4275fa4f5153e0fc8fd17da79cc6bdaa3b91d13
Anderson Lopes <>
2023-10-21 16:26:19 +0200

    Understands types

--- main.go
+++ main.go
@@ -11,4 +11,12 @@ func main() {
 	fmt.Printf("Welcome to our %v booking application!\n", conferenceName)
 	fmt.Println("We have total of", conferenceTickets, "tickets, and", remainingTickets, "are still available!")
 	fmt.Println("Get your ticket here to attend")
+
+	var userName string
+	var userTicket int
+	// Ask user their name
+
+	userName = "Lopes"
+	userTicket = 10
+	fmt.Printf("The user %v booked the %v° ticket\n", userName, userTicket)
 }
